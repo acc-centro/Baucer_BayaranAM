26a0b338b45d749d0e10f03a2f6ebafaa6490573
THYFANNEY DAVID <101650684+Thyfdv@users.noreply.github.com>
2022-05-24 11:39:08 +0800

    Add files via upload

---
+++ Salinan_Pembayar.php
@@ -0,0 +1,43 @@
+<DOCTYPE html>
+    <html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>BAUCER BAYARAN AM</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">  
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js" crossorigin="anonymous"></script>
+    
+  <style>
+      @import url('https://rsms.me/inter/inter.css');
+      html { font-family: 'Inter', sans-serif; }
+
+      @supports (font-variation-settings: normal) {
+        html { font-family: 'Inter var', sans-serif; }
+        body { padding: 10px; }
+      }
+  </style>
+  <script></script>
+
+</head>
+
+<body>
+<!-- Page Header -->
+<header>
+    
+    <div class="p-5 text-center">
+        <h2 class="mb-1" style="font-size:11px"><b>KERAJAAN NEGERI SABAH</b></h2>
+        <h1 class="mb-1" style="font-size:18px"><b>BAUCER BAYARAN AM</b></h1>
+        <h3 class="mb-1" style="font-size:8px">TAHUN KEWANGAN</h3>
+    </div>
+    
+</header>
+</body>
