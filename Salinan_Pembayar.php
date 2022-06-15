83a1124217d0f60cb880985cfe2d8c19e4e96910
THYFANNEY DAVID <101650684+Thyfdv@users.noreply.github.com>
2022-06-15 16:23:18 +0800

    update form

--- Salinan_Pembayar.php
+++ Salinan_Pembayar.php
@@ -1,8 +1,6 @@
-<DOCTYPE html>
+<!DOCTYPE html>
     <html lang="en">
-
 <head>
-
 <meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,7 +8,6 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">  
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
-
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
@@ -19,43 +16,34 @@
   <style>
       @import url('https://rsms.me/inter/inter.css');
       html { font-family: 'Inter', sans-serif; }
-
       @supports (font-variation-settings: normal) {
         html { font-family: 'Inter var', sans-serif; }
         body { padding: 10px; }
       }
-
       body{
         padding:1cm;
        
       
       }
-
       table{
         font family: arial, sans-serif;
         border-collapse: collapse;
         width:1000;
         
       }
-
       td,th{
         border: 1px solid;
         text-align: left;
       }
-
       h3{
         margin: -8px;
       }
-
     
       
   </style>
   <script>
-
   </script>
-
 </head>
-
 <body>
 <!-- Page Header -->
 <header>
@@ -68,7 +56,6 @@
   <h3 class="mb-1 text-right" style="font-size:8px"><b>JBN 38</b><br>(Pin 1/2022)</h3> 
 </header>
 <!--end header-->
-
 <!--table form-->
 <table>
     <tr> 
@@ -82,7 +69,6 @@
     <tr>
       <td colspan="8" rowspan="1" width="100" height="10" style="font-size:8px"> NAMA PENERIMA </td>
     </tr>
-
     <tr>
       <td  colspan="6" rowspan="2"width="100" height="10" style="font-size:8px"> ALAMAT/NAMA BANK </td>
       <td  colspan="2" rowspan="1" width="100" height="10" style="font-size:8px"> NO.KP/PASSPORT/SYARIKAT 
@@ -91,10 +77,9 @@
         </tr>
       </td>
     </tr>
-
     <tr>
       <td colspan="8" rowspan="5" width="100" height="10" style="font-size:8px"> PERIHAL BAYARAN </td>
-      <td colspan="1" rowspan="1" width="100" height="10" style="font-size:8px"> UNTUK KEGUNAAN PEJABAT </b>
+      <td colspan="1" rowspan="1" width="100" height="10" style="font-size:8px"> <b> UNTUK KEGUNAAN PEJABAT </b></td>
         <tr>
           <td colspan="1" rowspan="1" width="100" height="10" style="font-size:8px"> NO BAUCER PRB KOD JAB
             <tr>
@@ -112,7 +97,6 @@
         </tr>
       </td>
     </tr>
-
     <tr>
       <td colspan="1" rowspan="2" width="100" height="10" style="font-size:8px"> VOT..</td>
       <td colspan="1" rowspan="2" width="100" height="10" style="font-size:8px"> JAB..</td>
@@ -126,23 +110,19 @@
           <td colspan="1" rowspan="1" width="100" height="10"style="font-size:8px"> RM </td> 
         </tr>
     </tr>
-
     <tr>
       <td colspan="6" rowspan="1" width="100" height="10" style="font-size:8px"> </td>
       <td colspan="1" rowspan="1" width="100" height="10" style="font-size:8px"> 69,937.21</td>
       <td colspan="1" rowspan="1" width="100" height="10" style="font-size:8px"> DT </td>
     </tr>
-
     <tr>
       <td colspan="6" rowspan="1" width="100" height="10" style="font-size:8px"> JUMLAH BERSIH</td>
       <td colspan="1" rowspan="1" width="100" height="10" style="font-size:8px"> 69,937.21</td>
       <td colspan="1" rowspan="1" width="100" height="10" style="font-size:8px"> </td>
     </tr>
-
     <tr>
       <td colspan="8" rowspan="1" width="100" height="10" style="font-size:8px"> AMAUN (Dalam perkataan) RM: Enam Puluh Sembilan Ribu Sembilan Ratus Tiga Puluh Tujuh Dan Dua Puluh Satu Sen Sahaja</td>
     </tr>
-
     <tr>
       <td colspan="3" rowspan="1" width="100" height="10" style="font-size:8px"> AP 95 DIPATUHI </td>
       <td colspan="4" rowspan="1" width="100" height="10" style="font-size:8px"> AP 99 DAN AP 102 DIPATUHI </td>
@@ -157,26 +137,20 @@
       <td colspan="4" rowspan="1" width="100" height="10" style="font-size:8px"> Tandatangan Ringkas</td>
       <td colspan="4" rowspan="1" width="100" height="10" style="font-size:8px"> Tandatangan, Nama, Jawatan dan Jabatan </td>
     </tr>
-
     <tr>
       <td colspan="4" rowspan="2" width="100" height="10" style="font-size:8px"> Disemak oleh/ cap
        
       </td>
       <td colspan="4" rowspan="2" width="100" height="10" style="font-size:8px"> Tandatangan, Nama, Jawatan  </td>
     </tr>
-
     
     
        
       
    
  
-
-
   
-
     
-
 </table>
-
 </body>
+</html>
